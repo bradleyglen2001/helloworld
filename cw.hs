6af87f6d382c79c32ef2fcf52fd88495d3f4bd53
ba <bradleyglen2001@yahoo.com>
2019-05-01 17:05:11 +0100

    hskall :

---
+++ cw.hs
@@ -0,0 +1,13 @@
+
+
+fib 0 = 1
+fib 1 = fib 0 + 1
+fib x = fib (x -1) + fib (x -2)
+
+myfib = 1:1:zipWith (+) myfib (tail myfib)
+
+memoized_fib :: Int -> Integer
+memoized_fib = (map thefib [0 ..] !!)
+   where thefib 0 = 0
+         thefib 1 = 1
+         thefib n = memoized_fib (n-2) + memoized_fib (n-1)
