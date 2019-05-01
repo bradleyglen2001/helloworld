3d59c3ffa46b550dc8435d6b4486c1650db4e5a2
ba <bradleyglen2001@yahoo.com>
2019-05-01 17:16:44 +0100

    added a function called add

--- cw.hs
+++ cw.hs
@@ -6,8 +6,13 @@ fib x = fib (x -1) + fib (x -2)
 
 myfib = 1:1:zipWith (+) myfib (tail myfib)
 
+--this is a note
+--second note
 memoized_fib :: Int -> Integer
 memoized_fib = (map thefib [0 ..] !!)
    where thefib 0 = 0
          thefib 1 = 1
          thefib n = memoized_fib (n-2) + memoized_fib (n-1)
+
+
+add x y = x + y
